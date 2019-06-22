76ab611c86289df342657b3183c18df1757ad72d
Achim D. Brucker <adbrucker@0x5f.org>
2019-06-23 00:15:50 +0100

    Port to Isabelle 2019.

--- Hiding_Type_Variables.thy
+++ Hiding_Type_Variables.thy
@@ -1,5 +1,5 @@
 (***********************************************************************************
- * Copyright (c) 2018 Achim D. Brucker
+ * Copyright (c) 2018-2019 Achim D. Brucker
  *
  * All rights reserved.
  *
